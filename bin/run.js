0b3966c23267d50b0d4ecaa52d2fd79fe8b34f7a
jongleberry <me@jongleberry.com>
2017-11-11 07:01:23 +0000

    why

--- bin/run.js
+++ bin/run.js
@@ -36,7 +36,10 @@ if (program.shuffleMonitors || program.shuffle) options.shuffleMonitors = true
 
 runMonitors(monitorSetConfigs, options).exec().then((results) => {
   debug('%o', results)
-  if (results.every(result => result.success)) return process.exit(0)
+  if (results.every(result => result.success)) {
+    setImmediate(() => process.exit(0))
+    return
+  }
 
   console.error('Failing monitors detected!')
   process.exit(1)
